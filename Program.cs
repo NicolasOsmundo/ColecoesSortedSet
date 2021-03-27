331fc95a88ad80e3b606ce4b0610c3eec5266650
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-27 03:19:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ColecoesSortedSet
+{
+    public class Program
+    {
+			static void Main(string[] args)
+			{
+				// Criando uma lista ordenada
+				SortedSet<string> alunos = new SortedSet<string>();
+
+				// Adicionando elementos na lista
+				alunos.Add("Alberto");
+				alunos.Add("Giovanna");
+				alunos.Add("Fabio");
+				alunos.Add("Victor");
+				alunos.Add("Carlos");
+				Console.Write("Encontrou o aluno Carlos: ");
+
+				// Procurando na lista um determinado elemento
+				Console.WriteLine(alunos.Contains("Carlos"));
+				Console.WriteLine("");
+				foreach (string aluno in alunos)
+				{
+					Console.WriteLine(aluno);
+				}
+				Console.Read();
+			}
+		
+	}
+}
